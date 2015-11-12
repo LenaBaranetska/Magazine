e7a593690fb66c19363a7ec0d91fad7fd257a5a1
Sasha Guzik <andrii.marusiak@gmail.com>
2015-11-12 18:55:29 +0200

    Added new class Product

---
+++ Magazine/Infrastructure/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Magazine.Infrastructure
+{
+    public class Product
+    {
+        public string Nmae { get; set; }
+        public int Id { get; set; }
+        public double Price { get; set; }
+
+    }
+}
